6debad896c32038f7f5e5c0f60f3c5c55152843a
B-Rajinikanth <65448606+B-Rajinikanth@users.noreply.github.com>
2025-03-13 20:07:05 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
       <div className="flex flex-col h-screen justify-center items-center loading-screen">
         <img src={suhShortLogo} className='h-auto max-w-xs' alt="Sreenidhi University" />
         <p className='text-green-800 font-bold'>Loading...</p>
-        <p className='text-red-800 mt-2'>Please do not close, refresh, or press the back button.</p>
+        <p className='text-red-800 mt-2 mx-5'>Please do not close, refresh, or press the back button.</p>
       </div>
     )
   }
